1b69b92b95ec39ac9b1d36350fce7ba2839464c7
estudiante <estudiante@oretania-PC>
2025-10-17 10:14:32 +0200

    Version 4.0

--- Calculadora/src/com/ejemplo/calculadora/Calculadora.java
+++ Calculadora/src/com/ejemplo/calculadora/Calculadora.java
@@ -4,11 +4,12 @@ public class Calculadora {
 	public static void main(String[] args) {
 		Scanner sc=new Scanner(System.in);
 		double num1,num2;
+		int suma, resta, multiplicaion, division;
 		System.out.println("Introduce un número");
 		num1=sc.nextDouble();
 		System.out.println("Introduce otro número");
 		num2=sc.nextDouble();
-		
+		suma=Sumar(num1,num2);
 
 		
 	}
@@ -22,7 +23,12 @@ public class Calculadora {
 		return num1*num2;
 	}
 	
-		
+	public static double Dividir(double num1,double num2) {
+		if (num2 == 0) {
+			throw new IllegalArgumentException("No se puede dividir entre cero");
+			}
+		return num1/num2;
+	}
 	
 	
 }
