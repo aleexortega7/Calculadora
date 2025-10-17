4f456597a8d89530ac119c8cdef188f915a7a5d8
estudiante <estudiante@oretania-PC>
2025-10-17 10:00:53 +0200

    Version 1.0

---
+++ Calculadora/src/com/ejemplo/calculadora/Calculadora.java
@@ -0,0 +1,22 @@
+package com.ejemplo.calculadora;
+import java.util.Scanner;
+public class Calculadora {
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		double num1,num2;
+		System.out.println("Introduce un número");
+		num1=sc.nextDouble();
+		System.out.println("Introduce otro número");
+		num2=sc.nextDouble();
+		
+
+		
+	}
+	public static double Sumar(double num1,double num2) {
+		return num1+num2;
+	}
+	
+		
+	
+	
+}
