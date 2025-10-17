09f793812e41b8510546624daa4cca9210364b2c
estudiante <estudiante@oretania-PC>
2025-10-17 10:09:01 +0200

    Version 3.0

--- Calculadora/src/com/ejemplo/calculadora/Calculadora.java
+++ Calculadora/src/com/ejemplo/calculadora/Calculadora.java
@@ -18,6 +18,9 @@ public class Calculadora {
 	public static double Restar(double num1,double num2) {
 		return num1-num2;
 	}
+	public static double Multiplicar(double num1,double num2) {
+		return num1*num2;
+	}
 	
 		
 	
