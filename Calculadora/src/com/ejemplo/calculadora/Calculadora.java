0f99d147219b2aed788d4edb5d7a179fabf8fd9e
estudiante <estudiante@oretania-PC>
2025-10-17 10:06:37 +0200

    Version 2.0

--- Calculadora/src/com/ejemplo/calculadora/Calculadora.java
+++ Calculadora/src/com/ejemplo/calculadora/Calculadora.java
@@ -15,6 +15,9 @@ public class Calculadora {
 	public static double Sumar(double num1,double num2) {
 		return num1+num2;
 	}
+	public static double Restar(double num1,double num2) {
+		return num1-num2;
+	}
 	
 		
 	
